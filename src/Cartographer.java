4b1b6aabe8d3ab7439a4793e478cc00fae02cd41
BraeWebb <email@braewebb.com>
2018-06-03 15:21:26 +1000

    Inline Documentation

--- src/Cartographer.java
+++ src/Cartographer.java
@@ -11,17 +11,22 @@ import java.util.Map.Entry;
  */
 public class Cartographer extends Canvas {
 
-    private GraphicsContext graphics;
-    private Room start;
-
+    // Size (in pixels) of the rooms drawn to the map
     private static final int ROOM_SIZE = 50;
     private static final int ROOM_WIDTH = ROOM_SIZE / 2;
-    private static final int STROKE_WIDTH = 5;
+    // Width (in pixels) of the doors in the map
+    private static final int DOOR_WIDTH = 5;
 
+    // Canvas graphics renderer
+    private GraphicsContext graphics;
+
+    // Map cardinal directions to positions exits in that direction should be
+    // drawn relative to the rooms position
     private Map<String, int[]> exitPositions = new HashMap<>();
 
+    // Map of the current level
     private BoundsMapper map;
-
+    // Offsets to account for rooms in negative coordinates
     private int xOffset;
     private int yOffset;
 
@@ -34,28 +39,30 @@ public class Cartographer extends Canvas {
     public Cartographer(Room start) {
         super();
 
-        exitPositions.put("North", new int[]{ROOM_WIDTH, - STROKE_WIDTH,
-                ROOM_WIDTH, STROKE_WIDTH});
-        exitPositions.put("East", new int[]{ROOM_SIZE - STROKE_WIDTH,
-                ROOM_WIDTH, ROOM_SIZE + STROKE_WIDTH, ROOM_WIDTH});
-        exitPositions.put("West", new int[]{-STROKE_WIDTH, ROOM_WIDTH,
-                STROKE_WIDTH, ROOM_WIDTH});
+        // Load the positions exits should be drawn
+        exitPositions.put("North", new int[]{ROOM_WIDTH, -DOOR_WIDTH,
+                ROOM_WIDTH, DOOR_WIDTH});
+        exitPositions.put("East", new int[]{ROOM_SIZE - DOOR_WIDTH,
+                ROOM_WIDTH, ROOM_SIZE + DOOR_WIDTH, ROOM_WIDTH});
+        exitPositions.put("West", new int[]{-DOOR_WIDTH, ROOM_WIDTH,
+                DOOR_WIDTH, ROOM_WIDTH});
         exitPositions.put("South", new int[]{ROOM_WIDTH,
-                ROOM_SIZE - STROKE_WIDTH, ROOM_WIDTH,
-                ROOM_SIZE + STROKE_WIDTH});
+                ROOM_SIZE - DOOR_WIDTH, ROOM_WIDTH,
+                ROOM_SIZE + DOOR_WIDTH});
 
         graphics = getGraphicsContext2D();
-        this.start = start;
 
+        // Map out the map from the starting room
         map = new BoundsMapper(start);
         map.walk();
 
+        // Calculate and set the width and height of the canvas
         int width = (map.xMax - map.xMin + 1) * ROOM_SIZE;
         int height = (map.yMax - map.yMin + 1) * ROOM_SIZE;
-
         setWidth(width);
         setHeight(height);
 
+        // Calculate the offset to account for negative coordinates
         xOffset = Math.abs(map.xMin) * ROOM_SIZE;
         yOffset = Math.abs(map.yMin) * ROOM_SIZE;
     }
@@ -67,10 +74,14 @@ public class Cartographer extends Canvas {
      * @param location The location relative to the canvas to draw the room
      */
     private void drawRoom(Room room, Pair location) {
+        // Calculate the pixel position the room should be drawn
         int startX = (location.x * ROOM_SIZE) + xOffset;
         int startY = (location.y * ROOM_SIZE) + yOffset;
+
+        // Draw a rectangle representing the room
         graphics.strokeRect(startX, startY, ROOM_SIZE, ROOM_SIZE);
 
+        // Draw the doors for each room exit
         for (String direction : room.getExits().keySet()) {
             int[] position = exitPositions.get(direction);
             if (position == null) {
@@ -80,20 +91,26 @@ public class Cartographer extends Canvas {
                     position[2] + startX, position[3] + startY);
         }
 
+        // Draw representations for each of the items in the room
         for (Thing thing : room.getContents()) {
+            // Draw a player representation
             if (thing instanceof Player) {
                 graphics.strokeText("@", startX + 4, startY + 12);
             }
+            // Draw a treasure representation
             if (thing instanceof Treasure) {
                 graphics.strokeText("$", startX + 4 + ROOM_WIDTH,
                         startY + 12);
             }
+            // Draw a critter representation
             if (thing instanceof Critter) {
                 Critter critter = (Critter) thing;
                 if (critter.isAlive()) {
+                    // Draw an alive critter
                     graphics.strokeText("M", startX + 4,
                             startY + 12 + ROOM_WIDTH);
                 } else {
+                    // Draw a dead critter
                     graphics.strokeText("m", startX + 4 + ROOM_WIDTH,
                             startY + 12 + ROOM_WIDTH);
                 }
@@ -105,8 +122,10 @@ public class Cartographer extends Canvas {
      * Redraw the JavaFX display of the map.
      */
     public void update() {
+        // Clear the previous map view
         graphics.clearRect(0, 0, getWidth(), getHeight());
 
+        // Draw all the rooms in the map
         for (Entry<Room, Pair> entry : map.coords.entrySet()) {
             drawRoom(entry.getKey(), entry.getValue());
         }
--- src/CrawlGui.java
+++ src/CrawlGui.java
@@ -12,7 +12,6 @@ import javafx.stage.Stage;
 import java.util.List;
 import java.util.Optional;
 
-
 /**
  * JavaFX Application for a game where players travel through a map collecting
  * treasure and fighting monsters.
@@ -21,20 +20,22 @@ import java.util.Optional;
  */
 public class CrawlGui extends Application {
 
-    private GridPane directionButtons;
-    private GridPane actionButtons;
-    private Button[] buttons = new Button[10];
+    // Amount of direction buttons
     private static final int DIRECTION_COUNT = 4;
+
+    // Labels of the buttons to load
     private static final String[] BUTTONS = {
             "North", "West", "East", "South",
             "Look", "Examine", "Drop", "Take", "Fight", "Save"
     };
+    // Location in the grid of the buttons to load
     private static final Pair[] BUTTON_POSITIONS = {
             new Pair(1, 0), new Pair(0, 1), new Pair(2, 1),
             new Pair(1, 2), new Pair(0, 3), new Pair(1, 3),
             new Pair(0, 4), new Pair(1, 4), new Pair(0, 5),
             new Pair(0, 6)
     };
+    // Event handlers to call when buttons are pressed
     private final EventHandler[] BUTTON_CALLBACKS = {
             (event -> look()),
             (event -> examine()),
@@ -44,32 +45,64 @@ public class CrawlGui extends Application {
             (event -> save()),
     };
 
+    // Array of direction and action buttons
+    private Button[] buttons = new Button[10];
+    // Output area where messages should be displayed
     private TextArea output;
-
+    // Map of the current game level
     private Cartographer map;
+
+    // Room that a level start from
     private Room startRoom;
+    // Room the player is currently in
     private Room currentRoom;
+    // The player of the current game
     private Player player;
 
     /**
-     * Load all the buttons into the appropriate grid and add their
-     * action event handlers.
+     * Create a new grid pane and load direction buttons into the frame with
+     * their action event handlers.
+     *
+     * @return The grid the buttons are stored into
      */
-    private void loadButtons() {
+    private GridPane loadDirectionButtons() {
+        GridPane directionButtons = new GridPane();
         Pair buttonLocation;
+
+        // Create buttons and assign callbacks
         for (int i = 0; i < DIRECTION_COUNT; i++) {
             buttons[i] = new Button(BUTTONS[i]);
+            // Assign a button callback
             buttons[i].setOnAction((event) ->
                     move(((Button) event.getSource()).getText()));
+            // Add button to the grid
             buttonLocation = BUTTON_POSITIONS[i];
             directionButtons.add(buttons[i], buttonLocation.x, buttonLocation.y);
         }
+
+        return directionButtons;
+    }
+
+    /**
+     * Create a new grid pane and load action buttons into the frame with
+     * their action event handlers.
+     *
+     * @return The grid the buttons are stored into
+     */
+    private GridPane loadActionButtons() {
+        GridPane actionButtons = new GridPane();
+        Pair buttonLocation;
+
+        // Create buttons and assign callbacks
         for (int i = DIRECTION_COUNT; i < BUTTONS.length; i++) {
             buttons[i] = new Button(BUTTONS[i]);
+            // Assign a button callback
             buttons[i].setOnAction(BUTTON_CALLBACKS[i - DIRECTION_COUNT]);
+            // Add button to the grid
             buttonLocation = BUTTON_POSITIONS[i];
             actionButtons.add(buttons[i], buttonLocation.x, buttonLocation.y);
         }
+        return actionButtons;
     }
 
     /**
@@ -88,12 +121,15 @@ public class CrawlGui extends Application {
      * @return The string the user entered or null if there was no entry.
      */
     private String ask(String question) {
+        // Make a dialog input window
         TextInputDialog dialog = new TextInputDialog();
         dialog.setTitle(question);
         dialog.setHeaderText(null);
         dialog.setGraphic(null);
 
+        // Ask and wait for input
         Optional<String> result = dialog.showAndWait();
+        // Return input if given
         if (result.isPresent()){
             return result.get();
         }
@@ -110,17 +146,23 @@ public class CrawlGui extends Application {
      */
     private void move(String direction) {
         Room nextRoom = currentRoom.getExits().get(direction);
+        // Check a room in the direction exists
         if (nextRoom == null) {
             display("No door that way");
             return;
         }
+        // Attempt to leave the current room
         if (!currentRoom.leave(player)) {
             display("Something prevents you from leaving");
             return;
         }
+
+        // Move to the next room
         nextRoom.enter(player);
         currentRoom = nextRoom;
         display("You enter " + currentRoom.getDescription());
+
+        // Update the display
         map.update();
     }
 
@@ -130,10 +172,13 @@ public class CrawlGui extends Application {
      */
     private void look() {
         display(currentRoom.getDescription() + " - you see:");
+        // Display what items are found in the current room
         for (Thing thing : currentRoom.getContents()) {
             display(" " + thing.getShortDescription());
         }
         display("You are carrying:");
+        // Display what items are found in the current room
+        // and calculate the total worth of the items
         double worth = 0;
         for (Thing thing : player.getContents()) {
             display(" " + thing.getShortDescription());
@@ -150,11 +195,13 @@ public class CrawlGui extends Application {
      */
     private void examine() {
         String item = ask("Examine what?");
+        // Attempt to examine an item in the players inventory
         Thing thing = find(item, player.getContents());
         if (thing != null) {
             display(thing.getDescription());
             return;
         }
+        // Attempt to examine an item in the current room
         thing = find(item, currentRoom.getContents());
         if (thing != null) {
             display(thing.getDescription());
@@ -169,8 +216,10 @@ public class CrawlGui extends Application {
      */
     private void drop() {
         String item = ask("Item to drop?");
+        // Drop an item from the player
         Thing thing = player.drop(item);
         if (thing != null) {
+            // Add the item to the current room
             currentRoom.enter(thing);
         }
         map.update();
@@ -182,13 +231,18 @@ public class CrawlGui extends Application {
      */
     private void take() {
         String item = ask("Take what?");
+        // Find an item to take
         Thing thing = find(item, currentRoom.getContents(), true, false);
+
+        // Ensure that the thing can be taken and can leave
         if (thing instanceof Mob && ((Mob) thing).isAlive()) {
             return;
         }
         if (!currentRoom.leave(thing)) {
             return;
         }
+
+        // Collect the thing into the players inventory
         player.add(thing);
         map.update();
     }
@@ -201,18 +255,24 @@ public class CrawlGui extends Application {
      */
     private void fight() {
         String item = ask("Fight what?");
+        // Find an item to fight
         Thing thing = find(item, currentRoom.getContents(), false, true);
         Critter critter = (Critter) thing;
 
-        if (critter == null) {
+        // Check critter can be fought with
+        if (critter == null || !critter.isAlive()) {
             return;
         }
 
-        if (critter.isAlive()) {
-            player.fight(critter);
-        }
+        player.fight(critter);
+
+        // Display appropriate message
         if (critter.isAlive()) {
             display("Game over");
+            // Disable buttons when the game is over
+            for (Button button : buttons) {
+                button.setDisable(true);
+            }
         } else {
             display("You won");
         }
@@ -224,6 +284,7 @@ public class CrawlGui extends Application {
      */
     private void save() {
         String file = ask("Save filename?");
+        // Attempt to save from the start room to the given filename
         if (MapIO.saveMap(startRoom, file)) {
             display("Saved");
         } else {
@@ -256,12 +317,15 @@ public class CrawlGui extends Application {
     private Thing find(String description, List<Thing> contents,
                        boolean skip, boolean critter) {
         for (Thing thing : contents) {
+            // Skip over players
             if (skip && thing instanceof Player) {
                 continue;
             }
+            // Only count critters if critter flag is true
             if (critter && !(thing instanceof Critter)) {
                 continue;
             }
+            // Return a matching thing
             if (thing.getShortDescription().equals(description)) {
                 return thing;
             }
@@ -269,52 +333,65 @@ public class CrawlGui extends Application {
         return null;
     }
 
+    /**
+     * Launch the JavaFX Application.
+     *
+     * @param args Command line arguments passed to the program
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * Start the JavaFX interface by loading in the appropriate components.
+     *
+     * @param primaryStage The primary stage for this application,
+     *                     onto which the application scene can be set
+     */
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         List<String> parameters = getParameters().getRaw();
 
+        // Ensure a map filename has been provided
         if (parameters.size() != 1) {
             System.err.println("Usage: java CrawlGui mapname");
             System.exit(1);
         }
 
+        // Attempt to load the given filename
         Object[] data = MapIO.loadMap(parameters.get(0));
 
+        // Ensure the map can be successfully loaded
         if (data == null) {
             System.err.println("Unable to load file");
             System.exit(2);
         }
 
+        // Load appropriate data from the file into variables
         player = (Player) data[0];
-        startRoom = (Room) data[1];
-        currentRoom = startRoom;
+        currentRoom = startRoom = (Room) data[1];
         currentRoom.enter(player);
 
         BorderPane window = new BorderPane();
 
+        // Create the buttons pane on the right side
         VBox buttonPane = new VBox();
-        directionButtons = new GridPane();
-        actionButtons = new GridPane();
-        buttonPane.getChildren().add(directionButtons);
-        buttonPane.getChildren().add(actionButtons);
-
-        loadButtons();
-
+        buttonPane.getChildren().add(loadDirectionButtons());
+        buttonPane.getChildren().add(loadActionButtons());
         window.setRight(buttonPane);
 
+        // Create the output text area on the bottom side
         output = new TextArea("You find yourself in "
                 + currentRoom.getDescription());
         output.setEditable(false);
         window.setBottom(output);
 
+        // Create a map view into the center of the window
         map = new Cartographer(currentRoom);
         map.update();
         window.setCenter(map);
 
+        // Load and show the JavaFx scene
         primaryStage.setScene(new Scene(window));
         primaryStage.setTitle("Crawl - Explore");
         primaryStage.show();
